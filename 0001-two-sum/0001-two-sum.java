071acadba0de9460b255a005a77d71ab5f47ce66
FAIZAN FURQAN <115339180+mdfaizanofficial@users.noreply.github.com>
2024-06-25 11:16:36 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
         for(int i=0;i<nums.length-1;i++)
         {
             for(int j=i+1;j<nums.length;j++)
@@ -11,5 +12,6 @@ class Solution {
             }
         }
         return new int[]{};
+        
     }
 }
